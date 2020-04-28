cfd420330b9afde2f9aa94f5fa37b2300ed7456b
matthewK <matthew_kohjm@hotmail.sg>
2020-04-28 10:44:33 +0800

    navbar set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Layout } from 'antd';
 import { BrowserRouter as Router } from 'react-router-dom';
-import HeaderComponent from './LayoutComponents/Header';
+import Navbar from './LayoutComponents/Navbar';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <HeaderComponent />
-        {/* <AppRouter />
-        <FooterComponent /> */}
-      </Layout>
+      <Navbar />
     </Router>
   );
 }
