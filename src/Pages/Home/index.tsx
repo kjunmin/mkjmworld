1449a9fe59705e4882402d9241e7b2e5a2068e8d
matthewK <matthew_kohjm@hotmail.sg>
2020-04-29 18:27:42 +0800

    splash animation

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import HeaderSplash from '../../LayoutComponents/Header/HeaderSplash';
 import '../../css/styles.css';
 import TabBar from './TabBar';
 import AboutMe from './AboutMe';
+import JumbotronSplash from './JumbotronSplash';
 
 function Home() {
     return (
@@ -11,6 +11,9 @@ function Home() {
                 <div className="jumbotron-text-container">
                     <h1 className="jumbotron-main-text">The MKJM Progressive Web Application Project</h1>
                     <p className="jumbotron-sub-text">An experiment in building a modern fully functional Web Application.</p>
+                </div>
+                <div className="jumbotron-splash-container">
+                    <JumbotronSplash />
                 </div>
             </div>
             <TabBar />
